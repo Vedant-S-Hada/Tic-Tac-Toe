b6ad9add5f10545dac40fe4821abd9f6d17d6507
Vedant-S-Hada <vh8862@srmist.edu.in>
2026-04-20 09:24:27 +0530

    Added UC!

---
+++ Tic-Tac-Toe/src/UC1.java
@@ -0,0 +1,20 @@
+public class UC1 {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        // Initialize all cells with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print the board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
